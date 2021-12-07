3d9853fc1957cb50ff58a97479e404a51a4cc818
yongwoo <pure_yong@naver.com>
2021-12-07 13:18:11 +0900

    Add file extension

--- src/msg/MsgCode.java
+++ src/msg/MsgCode.java
@@ -1,7 +1,6 @@
 package msg;
 
 public class MsgCode {
-
 	public static String MSG_VALUE_DATE_FORMAT = "yyyyMMddHHmmss";
 	public static String MSG_CODE_FIELD_NAME = "NAME";
 	public static String MSG_CODE_STRING_NULL = "null";
@@ -9,6 +8,8 @@ public class MsgCode {
 	public static String MSG_CODE_STRING_NEW_LINE = "\r\n";
 	public static String MSG_CODE_FILE_PATH = "C:\\";
 	public static String MSG_CODE_FILE_SPLITER = "\t";
-	public static String MSG_CODE_FILE_EXTENSION_TEXT = ".txt";
-	public static String MSG_CODE_FILE_ENTENSION_EXCEL = ".xlsx";
+	public static String MSG_CODE_FILE_EXTENSION_TXT = ".txt";
+	public static String MSG_CODE_FILE_EXTENSION_XLSX = ".xlsx";
+	public static String MSG_CODE_FILE_EXTENSION_XLS = ".xls";
+	public static String MSG_CODE_FILE_EXTENSION_CSV = ".csv";
 }
