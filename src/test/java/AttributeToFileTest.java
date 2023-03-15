f26396d4bc42894194e53ec1155cdca806af9f0b
justdoanything <pure_yong@naver.com>
2023-03-16 01:01:05 +0900

    Test 함수 수정

--- src/test/java/AttributeToFileTest.java
+++ src/test/java/AttributeToFileTest.java
@@ -6,17 +6,28 @@ public class AttributeToFileTest {
     public static void main(String[] args) {
         try {
             AttributeToFile atf = AttributeToFile.builder("src/test/resources/question/ATF")
-                    .isWriteFile(true)
-                    .isGetString(true)
                     .splitter("|")
+                    .startWithLine(0)
+                    .isWriteFile(true)
+                    .isOpenFile(true)
+                    .writeFilePath("src/test/resources/question/ATF_2023.txt")
+                    .isGetString(true)
                     .build();
+            atf.addCodeMap("name", "code", "value");
             System.out.println(atf.parse());
 
-            AttributeToFile atfBuilder = new AttributeToFileBuilder("src/test/resources/question/ATF")
+            System.out.println("=============================================================");
+
+            AttributeToFile atfBuilder = new AttributeToFileBuilder("src/test/resources/question/ATF.xlsx")
+                    .splitter("\t")
+                    .startWithLine(0)
                     .isWriteFile(true)
+                    .isOpenFile(false)
+                    .writeFilePath("src/test/resources/question/ATF_2023.xlsx")
                     .isGetString(true)
-                    .splitter("|")
                     .build();
+            atfBuilder.addCodeMap("name", "code", "value");
+            System.out.println(atfBuilder.parse());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- src/test/java/FileToInsertQueryTest.java
+++ src/test/java/FileToInsertQueryTest.java
@@ -6,18 +6,25 @@ public class FileToInsertQueryTest {
   public static void main(String[] args) {
     try {
       FileToInsertQuery ftiq = FileToInsertQuery.builder("src/test/resources/question/FTIQ", "TEMP_TABLE")
-              .splitter("\t")
+              .splitter("|")
+              .startWithLine(0)
               .isWriteFile(true)
+              .isOpenFile(true)
+              .writeFilePath("src/test/resources/question/FTIQ_2023")
               .isGetString(true)
               .isBulkInsert(true)
+              .bulkInsertCnt(100)
               .build();
       System.out.println(ftiq.parse());
 
       System.out.println("================================================");
 
-      FileToInsertQuery ftiqBuilder = new FileToInsertQueryBuilder("src/test/resources/question/FTIQ", "TEMP_TABLE")
+      FileToInsertQuery ftiqBuilder = new FileToInsertQueryBuilder("src/test/resources/question/FTIQ.xlsx", "TEMP_TABLE")
               .splitter("\t")
+              .startWithLine(0)
               .isWriteFile(true)
+              .isOpenFile(false)
+              .writeFilePath("src/test/resources/question/FTIQ_2023.xlsx")
               .isGetString(true)
               .isBulkInsert(false)
               .build();
@@ -27,5 +34,3 @@ public class FileToInsertQueryTest {
     }
   }
 }
-
-
